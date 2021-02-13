beae6a765702e79737183e98e8d8682e3b3abb06
kristinau09 <kristinau09@gmail.com>
2021-02-13 14:31:58 -0600

    Sending messages to a message broker: ActiveMQ

---
+++ src/main/java/com/example/messageTesting/MessageTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageTestingApplication.class, args);
+	}
+
+}

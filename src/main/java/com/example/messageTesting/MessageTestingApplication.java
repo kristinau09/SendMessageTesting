7b9c3083e5717457004a422226255d2da3c38dac
kristinau09 <kristinau09@gmail.com>
2021-02-13 15:05:40 -0600

    Sending a message to the message broker through JMS

--- src/main/java/com/example/messageTesting/MessageTestingApplication.java
+++ src/main/java/com/example/messageTesting/MessageTestingApplication.java
@@ -2,12 +2,17 @@ package com.example.messageTesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 public class MessageTestingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessageTestingApplication.class, args);
+		ApplicationContext ctx=SpringApplication.run(MessageTestingApplication.class, args);
+		
+		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
+		jms.convertAndSend("MyQueue", "My first text message to the messsage broker");
 	}
 
 }

ec82855f2820c2ea75d62ab0c7e7397723437318
kristinau09 <kristinau09@gmail.com>
2021-02-17 11:27:19 -0600

    Refactoring

--- src/main/java/com/example/sendMessageTesting/SendMessageTestingApplication.java
+++ src/main/java/com/example/sendMessageTesting/SendMessageTestingApplication.java
@@ -1,4 +1,4 @@
-package com.example.messageTesting;
+package com.example.sendMessageTesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
-public class MessageTestingApplication {
+public class SendMessageTestingApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx=SpringApplication.run(MessageTestingApplication.class, args);
+		ApplicationContext ctx=SpringApplication.run(SendMessageTestingApplication.class, args);
 		
 		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
 		jms.convertAndSend("MyQueue", "Sending first text message to the messsage broker from the MessageTestingApplication.java, congrats!!!!!");
